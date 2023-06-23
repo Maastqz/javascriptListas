162eb6246b646f2098041352f49b211501659ff4
Maastqz <127257996+Maastqz@users.noreply.github.com>
2023-06-23 09:57:30 -0300

    Add files via upload

--- 3-operacoesAritmeticas.js
+++ 3-operacoesAritmeticas.js
@@ -2,4 +2,7 @@ console.log("Operaçoes aritméticas");
 
 console.log(2+2);
 console.log(10 + 8 * 2);
-console.log((10 = 8) * 2)
+console.log((10 + 8) * 2);
+
+console.log ("ano" + 2020);
+console.log("2" + "2");
