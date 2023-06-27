0beb26673bb30e6fbbcdd721d7db9880fb882793
Maastqz <127257996+Maastqz@users.noreply.github.com>
2023-06-27 11:47:56 -0300

    Add files via upload

--- 5-atribuicaoVariaveis.js
+++ 5-atribuicaoVariaveis.js
@@ -1,6 +1,12 @@
 console.log("Trabalhando com atribuição de variáveis")
 const idade = 29;
-const nome = "Ricardo";
+const primeiroNome = "Ricardo";
 const sobrenome = "Bugan";
 
-console.log(nome, sobrenome); //colocar virgula pois a virgula da espaço. Ou colocar assim:s
+console.log(nome, sobrenome); //colocar virgula pois a virgula da espaço. Ou colocar assim: console.log(nome + " " + sobrenome)
+console.log(`Meu nome é ${primeiroNome} ${sobrenome}`);
+
+let contador = 0
+contador = contador +1
+const nomeCompleto = primeiroNome + sobrenome;
+console.log(nomeCompleto);
